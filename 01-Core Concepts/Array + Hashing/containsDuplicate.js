551c8e69d4102990c76d219f275f7198c230e355
Mauaa <mauaa.datta@gmail.in>
2026-07-12 18:31:47 +0530

    Program: Contains Duplicate

---
+++ 01-Core Concepts/Array + Hashing/containsDuplicate.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 2, 3, 4, 4, 5];
+const seen = new Set();
+const duplicates = new Set();
+
+for (let i = 0; i < numbers.length; i++) {
+    if (seen.has(numbers[i])) {
+        duplicates.add(numbers[i])
+    }
+    seen.add(numbers[i])
+}
+
+console.log(seen); // [1, 2, 3, 4, 5]
+console.log(duplicates); // [2, 4]
