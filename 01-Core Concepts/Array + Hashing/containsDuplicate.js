905fe1f30b600648ebe5027a1798a0c62d75cea3
Mauaa <mauaa.datta@gmail.in>
2026-07-12 20:39:16 +0530

    Program: Two Sum

--- 01-Core Concepts/Array + Hashing/containsDuplicate.js
+++ 01-Core Concepts/Array + Hashing/containsDuplicate.js
@@ -11,3 +11,17 @@ for (let i = 0; i < numbers.length; i++) {
 
 console.log(seen); // [1, 2, 3, 4, 5]
 console.log(duplicates); // [2, 4]
+
+// Leetcode version -217
+var containsDuplicate = function (nums) {
+    const seen = new Set();
+    const duplicates = new Set();
+
+    for (let i = 0; i < nums.length; i++) {
+        if (seen.has(nums[i])) {
+            duplicates.add(nums[i])
+        }
+        seen.add(nums[i])
+    }
+    return duplicates.size > 0; // returns true or false
+};
---
+++ 01-Core Concepts/Array + Hashing/twoSum.js
@@ -0,0 +1,30 @@
+// Brute Force
+var twoSum = function(nums, target) {
+    // Loop through each element
+    for (let i = 0; i < nums.length; i++) {
+        // Loop through every element AFTER the current one
+        for (let j = i + 1; j < nums.length; j++) {
+            // Check if the pair sums to target
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+    // Return empty array if no solution found (though problem guarantees one)
+    return [];
+};   
+
+// using Map
+var twoSum = function (nums, target) {
+    const map = new Map(); // Stores: number -> index
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+
+    return [];
+};
